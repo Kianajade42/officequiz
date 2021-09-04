a340cec7b6e4b9d60176588941ed12c7759bf95d
Kiana Long <Kianajade42.kl@gmail.com>
2021-09-04 18:25:05 -0400

    mapping data/rendering

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,32 +1,89 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import {useState} from "react";
 
 
-class Question extends React.Component {
-//  questionBox = ({
-//   question, options, selected}) => {
-//    const [answer, setAnswer] = (options)
-
-  render(){
-  
+const Question = ({question, options, selected}) => {
+  const [answers, setAnswer] = useState(options);
   return (
     <div className="questionBox">
-      <div className="question">{this.props.question}</div>
-      {this.props.options.map((text, index) => (
+      <div className="question">"{question}"</div>
+      {answers.map((text, index) => (
         <button
           key={index}
           className="answerBtn"
-          // onClick={() => {
-          //     setAnswer([text]);
-          //     selected(text);
-          //}}
+          onClick={() => {
+              setAnswer([text]);
+              selected(text);
+          }}
         >
           {text}
         </button>
       ))}
     </div>
-  )};
-}
+  );
+};
+
+export default Question;
 
 
-export default Question
+
+
+
+//  const QuestionBox = ({question, options, selected}) => {
+//  const [answer, setAnswer] = useState(options);
+
+//export default class Question extends React.Component {
+//   export const Question = (options, selected) => {
+//      const setAnswer = {options};
+//   //render(){
+//     <div className="questionBox">
+//       {this.props.options.map((text, index) => (
+//         <button
+//           key={index}
+//           className="answerBtn"
+//           onClick={() => {
+//               setAnswer([text]);
+//               selected(text);
+//           }}
+//         >
+//           {text}
+//         </button>
+//       ))}
+//     </div>
+//   ;
+// //}
+// };
+
+
+
+
+
+// class Question extends React.Component { 
+  //  questionBox = ({
+//   question, options, selected}) => {
+//    const [answer, setAnswer] = (options)
+
+//   render(){
+  
+//   return (
+//     <div className="questionBox">
+//       <div className="question">{this.props.question}</div>
+//       {this.props.options.forEach((text, index) => (
+//         <button
+//           key={index}
+//           className="answerBtn"
+//           // onClick={() => {
+//           //     setAnswer([text]);
+//           //     selected(text);
+//           //}}
+//         >
+//           {text}
+//         </button>
+//       ))}
+//     </div>
+//   )};
+// }
+
+
+// export default Question;
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -9,12 +9,14 @@ class Quiz extends React.Component {
     constructor(props){
       super(props)
         this.state = {
-          questionBank: [],
+          questionBank: [this.props.quotes],
           score:  0,
           responses: 0
      }
     }
-  
+    //   componentDidMount() {
+    //     this.props.fetchQuestions()
+    //   }
      componentDidUpdate(prevProps){
        if (this.props.quotes !== prevProps.quotes)
        { 
@@ -48,27 +50,59 @@ class Quiz extends React.Component {
         });
     };
  
-
+ 
    render(){
+ 
 
      return ( 
         <div className="container">
         <div className="title">IT IS THE OFFICE QUIZ.</div>
         <div className="intro"> WHO SAID IT?</div>
-        <div className="questionBox">
-        {console.log(this.state.questionBank)}
-        {this.state.questionBank}
-        {this.state.questionBank.length > 0 && 
+        
+             {this.props.quotes.map((question, answers, id) => (
+                <React.Fragment>
+            <div className="questionBox">
+             <p  className="question" key={id}> {question.question}</p>
+             <p className="answerBtn" key={id}> {answers.answers}</p>
+             </div>
+                </React.Fragment>
+             ))}
+        
+       
+            {/* <div className="question">
+             {this.props.quotes.map((question, id) => (
+             <p className="questionBox" key={id}> {question.question}</p>
+                 ))}     
+             {this.props.quotes.map((answers, id) => (
+             <li className="answerBtn" key={id}> {answers.answers}</li>
+                ))} 
+          </div>
+            */}
+
+
+        {/* <div className="answerBtn">
+            {this.props.quotes[0].answers.split("  ")}
+             
+        </div> */}
+
+
+      {/* <div className="question">
+           {this.props.quotes[0].question}</div>
+        <div className="answerBtn">
+            {this.props.quotes[0].answers.split(" , ")}
+        </div> */}
+
+        {/* {this.state.questionBank.length > 0 && 
          this.state.responses < 5 &&
          this.state.questionBank.map(
            ({question, answers, correct, id}) => (
-        <Question question={question} options={answers.split(",")} key={id} 
+        <Question question={question} options={answers} key={id} 
         selected={answer => this.computeAnswer(answer,correct)}/>) 
         )}
-        {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>) : null}
+        {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>) : null} */}
          
         </div>
-    </div>
+   
         );    
          };
   
